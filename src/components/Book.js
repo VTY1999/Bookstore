476d3da68868e4e7171c86e7af42f83a22846eb9
verissimo francis <verissimo francis>
2022-03-09 13:08:44 +0100

    Add API to application

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,31 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/books';
+import { fetchBook, removeBook } from '../redux/books/books';
 
 const Book = () => {
+  const percentage = Math.floor(Math.random() * 90);
   const books = useSelector((state) => state.booksReducer);
   const dispatch = useDispatch();
-  const deleteBookFromStore = (id) => {
-    dispatch(removeBook(id));
-  };
+  useEffect(() => {
+    dispatch(fetchBook());
+  }, []);
   return (
-    <div>
-      <ul className="Books">
-        {books.map(({ id, title, author }) => (
-          <li key={id}>
-            <span>
-              {title}
-              {' '}
-            </span>
-            <span>
-              {author}
-              {' '}
-            </span>
-            <button type="button" onClick={() => deleteBookFromStore(id)}>Remove</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      {books.map((book) => (
+        <div key={book.item_id} className="card">
+          <div className="info">
+            <span className="book-category">{book.category}</span>
+            <h2>{book.title}</h2>
+            <span>{book.author}</span>
+            <div className="container">
+              <ul>
+                <li>
+                  <button id={book.item_id} type="button">
+                    Comments
+                  </button>
+                </li>
+                <li>
+                  <button id={book.item_id} type="button">
+                    Edit
+                  </button>
+                </li>
+                <li>
+                  <button id={book.item_id} type="button" onClick={() => dispatch(removeBook(book.item_id))}>
+                    Remove
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="progress">
+            <span>{`${percentage}%`}</span>
+            <span>completed</span>
+          </div>
+          <div className="view-progress">
+            <span>Current chapter</span>
+            <h3>Chapter 2</h3>
+            <button id={book.item_id} type="button">Update progress</button>
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
 
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,23 +1,78 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const FETCH_BOOK = 'bookStore/books/FETCH_BOOK';
 
 const initialState = [];
 
-export const addBook = (payload) => ({
+const returnBook = (payload) => ({
   type: ADD_BOOK,
   payload,
 });
-export const removeBook = (payload) => ({
-  type: REMOVE_BOOK,
-  payload,
-});
+
+export const addBook = (newBook) => async (dispatch) => {
+  const response = await fetch(
+    'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/kQDJnBa8pVnQ9r9R8wZR/books/',
+    {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(newBook),
+    },
+  );
+
+  if (response.ok) {
+    dispatch(returnBook(newBook));
+  }
+};
+
+export const removeBook = (id) => async (dispatch) => {
+  const body = {
+    item_id: id,
+  };
+  const response = await fetch(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/kQDJnBa8pVnQ9r9R8wZR/books/${id}`,
+    {
+      method: 'DELETE',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(body),
+    });
+  if (response.ok) {
+    dispatch({
+      type: REMOVE_BOOK,
+      id,
+    });
+  }
+};
+
+export const fetchBook = () => async (dispatch) => {
+  const response = await fetch(
+    'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/kQDJnBa8pVnQ9r9R8wZR/books',
+  );
+  const result = await response.json();
+  const loadData = Object.entries(result);
+  loadData.map(([key, value]) => {
+    const { title, category } = value[0];
+    return {
+      item_id: key,
+      title,
+      category,
+    };
+  });
+
+  if (loadData) {
+    dispatch({
+      type: FETCH_BOOK,
+      payload: loadData,
+    });
+  }
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
+    case FETCH_BOOK:
+      return action.payload;
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload);
+      return state.filter((book) => book.item_id !== action.id);
     default:
       return state;
   }
